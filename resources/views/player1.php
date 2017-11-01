ea5622f65db44cf2bd0f641c5ac79e816f7a1b2d
veljkodjokic <veledjokic69@gmail.com>
2017-11-01 18:44:19 +0100

    hls proof

--- resources/views/player1.php
+++ resources/views/player1.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
-<html lang="">
+<html lang="{{ app()->getLocale() }}">
 <head>
 
     <!-- CSRF Token -->
-    
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title></title>
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Styles -->
-    
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
@@ -25,7 +25,9 @@
                     </button>
 
                     <!-- Branding Image -->
-                    <
+                    <a class="navbar-brand" href="{{ url('/') }}">
+                        {{ config('app.name', 'Laravel') }}
+                    </a>
                 </div>
 
                 <div class="collapse navbar-collapse" id="app-navbar-collapse">
@@ -37,7 +39,30 @@
                     <!-- Right Side Of Navbar -->
                     <ul class="nav navbar-nav navbar-right">
                         <!-- Authentication Links -->
-                        
+                        @guest
+                            <li><a href="{{ route('login') }}">Login</a></li>
+                            <li><a href="{{ route('register') }}">Register</a></li>
+                        @else
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+
+                                <ul class="dropdown-menu">
+                                    <li>
+                                        <a href="{{ route('logout') }}"
+                                            onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                            Logout
+                                        </a>
+
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                            {{ csrf_field() }}
+                                        </form>
+                                    </li>
+                                </ul>
+                            </li>
+                        @endguest
                     </ul>
                 </div>
             </div>
@@ -49,7 +74,10 @@
                 <div class="panel-heading">Dashboard</div>
 
                 <div class="panel-body">
-                    @v>
+                    @if (session('status'))
+                        <div class="alert alert-success">
+                            {{ session('status') }}
+                        </div>
                     @endif
 
                     <script src="https://cdn.jsdelivr.net/npm/hls.js@latest"></script>
