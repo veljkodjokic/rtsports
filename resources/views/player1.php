71971b8e3c70ec0f3084519148f4302a7dc25ffb
veljkodjokic <veledjokic69@gmail.com>
2017-11-01 19:12:16 +0100

    hls proof

--- resources/views/player1.php
+++ resources/views/player1.php
@@ -80,24 +80,28 @@
                         </div>
                     @endif
 
-                    <script src="https://cdn.jsdelivr.net/npm/hls.js@latest"></script>
-<video id="video"></video>
-<script>
-  if(Hls.isSupported()) {
-    var video = document.getElementById('video');
-    var hls = new Hls();
-    hls.loadSource('http://91.121.72.155/hls/stream.m3u8');
-    hls.attachMedia(video);
-    hls.on(Hls.Events.MANIFEST_PARSED,function() {
-      video.play();
-  });
- }
-</script>
+                    <video id="video" width=960 height=540 controls>
+                        <source src="http://91.121.72.155/hls/stream.m3u8" type="application/x-mpegURL">
+                    </video>
                 </div>
             </div>
         </div>
     </div>
 </div>
+
+<script src="https://cdn.jsdelivr.net/npm/hls.js@latest"></script>
+
+<script type="text/javascript">
+    if(Hls.isSupported()) {
+        var video = document.getElementById('video');
+        var hls = new Hls();
+        hls.loadSource('http://91.121.72.155/hls/stream.m3u8');
+        hls.attachMedia(video);
+        hls.on(Hls.Events.MANIFEST_PARSED,function() {
+            video.play();
+        });
+    }
+</script>
 </div>
 
     <!-- Scripts -->
--- resources/views/player2.php
+++ resources/views/player2.php
@@ -73,22 +73,35 @@
             <div class="panel panel-default">
                 <div class="panel-heading">Dashboard</div>
 
-                <script src="https://cdn.jsdelivr.net/npm/hls.js@latest"></script>
-<video id="video"></video>
-<script>
-  if(Hls.isSupported()) {
-    var video = document.getElementById('video');
-    var hls = new Hls();
-    hls.loadSource('https://video-dev.github.io/streams/x36xhzz/x36xhzz.m3u8');
-    hls.attachMedia(video);
-    hls.on(Hls.Events.MANIFEST_PARSED,function() {
-      video.play();
-  });
- }
-</script>
+                <div class="panel-body">
+                    @if (session('status'))
+                        <div class="alert alert-success">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    <video id="video" width=960 height=540 controls>
+                        <source src="http://91.121.72.155/hls/stream1.m3u8" type="application/x-mpegURL">
+                    </video>
+                </div>
             </div>
         </div>
     </div>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/hls.js@latest"></script>
+
+<script type="text/javascript">
+    if(Hls.isSupported()) {
+        var video = document.getElementById('video');
+        var hls = new Hls();
+        hls.loadSource('http://91.121.72.155/hls/stream1.m3u8');
+        hls.attachMedia(video);
+        hls.on(Hls.Events.MANIFEST_PARSED,function() {
+            video.play();
+        });
+    }
+</script>
 </div>
 </div>
 </body>
