0a0cf78894fe405d88aed93bd8205a7975224e31
veljkodjokic <veledjokic69@gmail.com>
2017-11-01 17:43:15 +0100

    hls proof

--- resources/views/player2.blade.php
+++ resources/views/player2.blade.php
@@ -74,12 +74,6 @@
                 <div class="panel-heading">Dashboard</div>
 
                 <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
                     <video id="video" width=960 height=540 controls>
                         <source src="http://91.121.72.155/hls/stream1.m3u8" type="application/x-mpegURL">
                     </video>
