93f9010fcc1ce5d63a6b0a437bb40eacadf4d372
veljkodjokic <veledjokic69@gmail.com>
2017-11-01 19:20:34 +0100

    hls proof

--- resources/views/player2.blade.php
+++ resources/views/player2.blade.php
@@ -81,7 +81,7 @@
                     @endif
 
                     <video id="video" width=960 height=540 controls>
-                        <source {!!('src="http://91.121.72.155/hls/stream1.m3u8"')!!} type="application/x-mpegURL">
+                        <source src="http://91.121.72.155/hls/stream1.m3u8" type="application/x-mpegURL">
                     </video>
                 </div>
             </div>
@@ -89,19 +89,7 @@
     </div>
 </div>
 
-<script src="https://cdn.jsdelivr.net/npm/hls.js@latest"></script>
 
-<script type="text/javascript">
-    if(Hls.isSupported()) {
-        var video = document.getElementById('video');
-        var hls = new Hls();
-        hls.loadSource('http://91.121.72.155/hls/stream1.m3u8');
-        hls.attachMedia(video);
-        hls.on(Hls.Events.MANIFEST_PARSED,function() {
-            video.play();
-        });
-    }
-</script>
 </div>
 </div>
 </body>
