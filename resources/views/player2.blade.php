b4017c4413c12ed57f9ac6ba88daf26a5d0e1c08
veljkodjokic <veledjokic69@gmail.com>
2017-11-02 13:21:12 +0100

    Complete SSL Update

--- resources/views/player2.blade.php
+++ resources/views/player2.blade.php
@@ -81,7 +81,7 @@
                     @endif
 
                     <video id="video" width=960 height=540 controls>
-                        <source src="https://www.rtsportsbackend.work/hls/stream.m3u8" type="application/x-mpegURL">
+                        <source src="https://www.rtsportsbackend.work/hls/stream1.m3u8" type="application/x-mpegURL">
                     </video>
                 </div>
             </div>
