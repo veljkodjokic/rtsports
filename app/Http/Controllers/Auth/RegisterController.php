bab19d55cff0006cef3cc2b73a23e5c7940d19c2
veljkodjokic <veledjokic69@gmail.com>
2017-11-05 14:34:38 +0100

    Mail verification

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -7,7 +7,6 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
-use App\User;
 use Mail;
 use App\Mail\veirifyEmail;
 
