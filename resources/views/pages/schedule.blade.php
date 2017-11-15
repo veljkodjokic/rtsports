1e3cb219c36ce063f42f86b115b85c1edc135d86
veljkodjokic <veledjokic69@gmail.com>
2017-11-15 23:40:08 +0100

    schedule update

--- resources/views/pages/schedule.blade.php
+++ resources/views/pages/schedule.blade.php
@@ -5,7 +5,7 @@
 
   <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.5.0/css/bootstrap-datepicker.css" rel="stylesheet">
 
-  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
 
   <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.5.0/js/bootstrap-datepicker.js"></script>
 
