60469d59c3583396eb373608b171a48bd5341efd
veljkodjokic <veledjokic69@gmail.com>
2017-11-23 13:21:24 +0100

    Vod update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -65,13 +65,13 @@
 
                             <li><a href="/channels">Channels</a></li>
 
-                            <li><a href="/shows">TV Shows</a></li>
-
                             <li><a href="/schedule">Schedule</a></li>
 
                             <li><a href="/contact">Contact us</a></li>
 
                             <li><a href="/about">About us</a></li>
+
+                            <li><a href="/shows">VoD Test</a></li>
 
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
