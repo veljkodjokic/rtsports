faec9de511a7ddb89f328ee1a32d17fbaa677d4a
veljkodjokic <veledjokic69@gmail.com>
2017-12-03 03:45:41 +0100

    User Interface update

--- resources/views/partials/channel_meni_content.blade.php
+++ resources/views/partials/channel_meni_content.blade.php
@@ -16,8 +16,36 @@
 </style>
 @foreach($streams as $theStream)
 
-    <div id="kvadrat" style="margin: 0 auto;  width: 100%; max-width: 800px; height: 160px; margin-bottom: -20px;">
-            <a  href="/game{{ $theStream->id }}"><img src="pics/game{{ $theStream->id }}.png" style="width:100%; height:100%">@if($theStream->Live())<div id="live_stream"><b>&#9673;LIVE</b></div>@endif</a>
-    </div><br>
+    @if($theStream->upNext())
+    @php
+        $event=$theStream->upNext();
+
+        $team1=App\Team::where('team',$event['team1'])->get()->first();
+        $team2=App\Team::where('team',$event['team2'])->get()->first();
+
+        $name1=$team1['name'];
+        $name2=$team2['name'];
+
+        $time=Carbon\Carbon::parse($event['time'])->format('h:i');
+        $day=Carbon\Carbon::parse($event['day'])->format('m/d');
+    @endphp
+        <div id="kvadrat" style="margin: 0 auto;  width: 100%; max-width: 800px; height: 160px; margin-bottom: -20px;">
+            <a id="game-link" href="/game{{$theStream->id}}">
+                <div id="team-left">
+                    <div id="team-logo" style="margin-bottom: 15px;"><img src="/pics/teams/{{ $event['team1'] }}.png" id="logo-img"></div>
+                    <div id="team-name">{{ $name1 }}</div>
+                </div>
+                <div id="game-info">
+                    <div id="game-time">{{ $time }}<raw style="font-size: 15pt; font-weight: 15;">UTC+1</raw></div>
+                    <div id="game-date">{{ $day }}</div>
+                </div>
+                <div id="team-rigth">
+                    <div id="team-logo" style="margin-bottom: 15px;"><img src="/pics/teams/{{ $event['team2'] }}.png" id="logo-img"></div>
+                    <div id="team-name">{{ $name2 }}</div>
+                </div>
+            </a>
+        </div> 
+        <br>
+    @endif
 
 @endforeach
