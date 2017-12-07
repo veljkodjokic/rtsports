5d60a98b679ac7c22b14a5719b8967d61883f890
veljkodjokic <veledjokic69@gmail.com>
2017-12-07 09:20:08 +0100

    news update

--- resources/views/partials/channel_meni_content.blade.php
+++ resources/views/partials/channel_meni_content.blade.php
@@ -52,8 +52,10 @@
 
 @endforeach
 
-<div id="kvadrat" style="margin: 0 auto;  width: 100%; max-width: 800px; height: 160px; margin-bottom: -20px;">
-    <a href="/game8">
-        <img src="/pics/nbatv.png" style="width: 100%; height: 100%;">
-    </a>
-</div> 
+@if(!$stream->id==8)
+    <div id="kvadrat" style="margin: 0 auto;  width: 100%; max-width: 800px; height: 160px; margin-bottom: -20px;">
+        <a href="/game8">
+            <img src="/pics/nbatv.png" style="width: 100%; height: 100%;">
+        </a>
+    </div> 
+@endif
