14bcf60d91d186cf9097cb49d7c1bd2d25c46050
veljkodjokic <veledjokic69@gmail.com>
2017-12-09 14:01:56 +0100

    fef

--- config/services.php
+++ config/services.php
@@ -15,8 +15,8 @@ return [
     */
 
     'mailgun' => [
-        'domain' => 'mail.rtsportsbackend.work',
-        'secret' => 'bullshit1',
+        'domain' => env('MAILGUN_DOMAIN'),
+        'secret' => env('MAILGUN_SECRET'),
     ],
 
     'ses' => [
