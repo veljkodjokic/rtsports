257392f2d6012390ad31d2c87a69594d8c1d7724
veljkodjokic <veledjokic69@gmail.com>
2018-01-24 09:55:50 +0100

    dd

--- resources/views/player.blade.php
+++ resources/views/player.blade.php
@@ -34,6 +34,8 @@
     </div>
 </div>
 <div class="side_players_bottom">@include('partials.channel_meni_content')</div>
+
+<script src="https://cdn.jsdelivr.net/npm/hls.js@latest"></script>
 <script>
   if(Hls.isSupported()) {
     var video = document.getElementById('video');
